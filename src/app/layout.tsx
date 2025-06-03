928af659c3bf9549db9c47db1bc870efd25cf49b
darioHer <105765314+darioHer@users.noreply.github.com>
2025-06-03 17:25:56 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 'use client';
 
 import "../styles/globals.css";
-import { usePathname } from 'next/navigation';
 import Navigation from '../components/layout/Navigation';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-
   return (
     <html lang="es">
       <body>
-        {/* Navigation ahora internamente decide si mostrar Sidebar */}
         <Navigation />
 
         <div className="flex flex-col lg:flex-row gap-6 px-4 py-6">
